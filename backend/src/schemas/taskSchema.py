b98fffb0795edd5aff6532335895edc31eb988ba
Balaram Pai <balaram151023@gmail.com>
2026-06-23 19:48:36 +0530

    Validtation_for_RequestSchema_and_UpdateSchema_and_UpdateStatusSchema

--- backend/src/schemas/taskSchema.py
+++ backend/src/schemas/taskSchema.py
@@ -1,13 +1,21 @@
 # src/schemas/taskSchema.py
 
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from datetime import datetime,date
 
+# Helper Enums Class (i.e. Datatype for Validation of Status like Field from pydantic for other fields.)
+from enum import StrEnum
+class AllowedStatus(StrEnum):
+    PENDING = "PENDING"
+    COMPLETED = "COMPLETED"
+    IN_PROGRESS = "IN_PROGRESS"
+
+
 class TaskCreateRequestSchema(BaseModel):
-    title : str
-    description : str | None = None
-    duration : int
-    location : str
+    title : str = Field(min_length=3,max_length=100)
+    description : str | None = Field(default=None,max_length=500)
+    duration : int = Field(gt=0)
+    location : str = Field(min_length=3,max_length=100)
     dueDate : date
     
     
@@ -23,13 +31,13 @@ class TaskResponseSchema(BaseModel):
     
     
 class TaskUpdateRequestScehma(BaseModel):
-    title: str | None = None
-    description: str | None = None
-    duration: int | None = None
-    location: str | None = None
+    title: str | None = Field(default=None,min_length=3,max_length=100)
+    description: str |None = Field(default=None,max_length=500)
+    duration: int | None = Field(default=None,gt=0)
+    location: str | None = Field(default=None,min_length=3,max_length=100)
     dueDate: date | None = None
    
     
 class TaskStatusUpdateRequestSchema(BaseModel):
-    status: str
+    status: AllowedStatus
     
